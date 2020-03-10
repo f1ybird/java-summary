f3d9bb12e5e9185f1d95e027640cd3e2301d22d4
zhangkai <kevin_love_it@163.com>
2020-03-11 00:00:36 +0800

    deadLock分析

---
+++ src/main/java/juc/CountDownLatchTest.java
@@ -0,0 +1,13 @@
+package juc;
+
+public class CountDownLatchTest {
+
+    public static void main(String[] args) {
+//        CountDownLatch countDownLatch = new CountDownLatch(3);
+//        CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
+//        Semaphore semaphore = new Semaphore(2);
+
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
