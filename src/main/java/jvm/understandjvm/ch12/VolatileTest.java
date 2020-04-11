6f9f623c2c1d640536ae81075c3ba05b82279ff4
zhangkai <kevin_love_it@163.com>
2020-04-12 00:51:19 +0800

    :memo:高效并发测试代码

---
+++ src/main/java/jvm/understandjvm/ch12/VolatileTest.java
@@ -0,0 +1,36 @@
+package jvm.understandjvm.ch12;
+
+/**
+ * volatile变量自增运算测试
+ *
+ * @author zzm
+ */
+public class VolatileTest {
+
+    public static volatile int race = 0;
+
+    public static void increase() {
+        race++;
+    }
+
+    private static final int THREADS_COUNT = 20;
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[THREADS_COUNT];
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            threads[i] = new Thread(() -> {
+                for (int i1 = 0; i1 < 10000; i1++) {
+                    increase();
+                }
+            });
+            threads[i].start();
+        }
+
+        // 等待所有累加线程都结束
+        while (Thread.activeCount() > 1) {
+            Thread.yield();
+        }
+
+        System.out.println(race);
+    }
+}
---
+++ src/main/java/jvm/understandjvm/ch13/VectorTestCase_2.java
@@ -0,0 +1,50 @@
+package jvm.understandjvm.ch13;
+
+import java.util.Vector;
+
+/**
+ * 增加锁确保线程安全
+ *
+ * @author zzm
+ */
+public class VectorTestCase_2 {
+
+    private static Vector<Integer> vector = new Vector<Integer>();
+
+    public static void main(String[] args) {
+        while (true) {
+            for (int i = 0; i < 10; i++) {
+                vector.add(i);
+            }
+
+            Thread removeThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    synchronized (vector) {
+                        for (int i = 0; i < vector.size(); i++) {
+                            vector.remove(i);
+                        }
+                    }
+                }
+            });
+
+            Thread printThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    synchronized (vector) {
+                        for (int i = 0; i < vector.size(); i++) {
+                            System.out.println((vector.get(i)));
+                        }
+                    }
+                }
+            });
+
+            removeThread.start();
+            printThread.start();
+
+            //不要同时产生过多的线程，否则会导致操作系统假死
+            while (Thread.activeCount() > 20) ;
+        }
+    }
+
+}
