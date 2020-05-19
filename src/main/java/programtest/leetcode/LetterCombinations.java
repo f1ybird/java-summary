4bfc3adb4ae295e8786de3e7a0c6dd687d3ee102
zhangkai <kevin_love_it@163.com>
2020-05-19 23:02:21 +0800

    :memo:leetcode测试

---
+++ src/main/java/programtest/leetcode/LetterCombinations.java
@@ -0,0 +1,67 @@
+package programtest.leetcode;
+
+import java.util.*;
+
+/**
+ * @Description: 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ * <p>
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入："23"
+ * 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 说明:
+ * 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+ * @Author: zhangkai
+ * @Date: 2020/5/19 21:45
+ */
+public class LetterCombinations {
+
+    public static void main(String[] args) {
+        LetterCombinations combinations = new LetterCombinations();
+        List<String> strings = combinations.letterCombinations("23");
+        strings.forEach(System.out::println);
+    }
+
+    public Map<String, String> phone = new HashMap<String, String>() {{
+        put("2", "abc");
+        put("3", "def");
+        put("4", "ghi");
+        put("5", "jkl");
+        put("6", "mno");
+        put("7", "pqrs");
+        put("8", "tuv");
+        put("9", "wxyz");
+    }};
+
+    List<String> output = new ArrayList<String>();
+
+    public void backtrack(String combination, String next_digits) {
+        // if there is no more digits to check
+        if (next_digits.length() == 0) {
+            // the combination is done
+            output.add(combination);
+        }
+        // if there are still digits to check
+        else {
+            // iterate over all letters which map
+            // the next available digit
+            String digit = next_digits.substring(0, 1);
+            String letters = phone.get(digit);
+            for (int i = 0; i < letters.length(); i++) {
+                String letter = phone.get(digit).substring(i, i + 1);
+                // append the current letter to the combination
+                // and proceed to the next digits
+                backtrack(combination + letter, next_digits.substring(1));
+            }
+        }
+    }
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() != 0) {
+            backtrack("", digits);
+        }
+        return output;
+    }
+}
